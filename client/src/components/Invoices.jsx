c8a9ae88005ada8e0ed7f5c29f0caa0690a47385
SmileAndWaveBoyz <44876348+SmileAndWaveBoyz@users.noreply.github.com>
2024-01-09 20:01:07 +0000

    Row CSS

--- client/src/components/Invoices.jsx
+++ client/src/components/Invoices.jsx
@@ -4,14 +4,19 @@ function Invoices() {
   return (
     <main className='container invoices'>
         <section className='invoices__row'>
+          <div className="invoices_left">
             <h3 className='invoices__id'><h3 className='invoices__idH'>#</h3>RT3080</h3> 
-            <div className="">
+            <div className="invoices_dateContainer">
                 <p className='invoices__date due'>Due</p> <p className='invoices__date'> 19 Aug 2021</p> 
             </div>
-            <p className='invoices__name'>Jensen Huang</p>
             <h3 className='invoices__id'>£ 1,800.90</h3>
+          </div>
+
+          <div className="invoices_right">
+            <p className='invoices__name'>Jensen Huang</p>
             <div className="invoices__status paid"><svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8" fill="none"> <circle className='invoices__status-circle green' cx="4" cy="4" r="4" /></svg> Paid</div>
-            <svg className='mobileNoDisplay' xmlns="http://www.w3.org/2000/svg" width="7" height="10" viewBox="0 0 7 10" fill="none"> <path d="M1 1L5 5L1 9" stroke="#7C5DFA" stroke-width="2"/></svg>
+            <svg className='mobileNoDisplay invoices__arrow' xmlns="http://www.w3.org/2000/svg" width="7" height="10" viewBox="0 0 7 10" fill="none"> <path d="M1 1L5 5L1 9" stroke="#7C5DFA" stroke-width="2"/></svg>
+          </div>
         </section>
     </main>
   )
